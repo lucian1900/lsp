da12c87abe01fe6361a89b1bc55941abc543f59e
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-03-29 11:40:46 +0100

    Reader works. Beginning of eval.

--- lsp.py
+++ lsp.py
@@ -1,9 +1,61 @@
-def parse(s):
+class Symbol(str): pass
+class List(list): pass
 
-	return result
+def eval(sexp):
+	try:
+		if len(sexp) == 0:
+			raise ValueError("Missing function expression")
+	except:
 
-def tokenize(s):
+	
+	return sexp
+
+def read(tokens):
+	'''Lisp reader
+
+	>>> read(['1'])
+	1
+
+	>>> read(['(', '1', '2', ')'])
+	[1, 2]
+
+	>>> read(['(', '+', '1', '2', ')'])
+	['+', 1, 2]
 	'''
+
+	try:
+		tok = tokens[0]
+		tokens = tokens[1:]
+	except IndexError:
+		raise SyntaxError("Unexpected EOF")
+
+	if tok == '(':
+		import pdb; pdb.set_trace()
+		try:
+			end = tokens.index(')')
+		except ValueError:
+			raise SyntaxError("Expected ')'")
+
+		in_toks = tokens[:end]
+
+		exp = List()
+		while in_toks: # depends on mutation inside parse(). nasty
+			exp.append(read(in_toks))
+			exp.pop(0)
+
+		return exp
+
+	try:
+		atom = int(tok)
+	except ValueError:
+		return Symbol(tok)
+	else:
+		return atom # int after all
+
+def tokenize(source):
+	'''Split by parens, consider commas whitespace
+	Inspired by Peter Norvig's lis.py
+
 	>>> tokenize("(1, 2)")
 	['(', '1', '2', ')']
 	'''
@@ -15,9 +67,9 @@ def tokenize(s):
 	}
 
 	for tok, rep in tokens.items():
-		s = s.replace(tok, rep)
+		source = source.replace(tok, rep)
 
-	return s.split()
+	return source.split()
 
 if __name__ == '__main__':
 	import doctest
