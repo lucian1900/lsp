b28e906403f95ff8b370bb527609cb5cc2034aef
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-03-30 08:28:47 +0100

    Whitespace cleanup.

--- lsp.py
+++ lsp.py
@@ -4,72 +4,72 @@ class Atom(object): pass
 class Number(Atom, int): pass
 class String(Atom, str): pass
 
+
 funs = {
-	'+': sum,
+    '+': sum,
 }
 
+
 def eval(sexp):
-	if isinstance(sexp, List):
-		if len(sexp) == 0:
-			raise ValueError("Missing function expression")
+    if isinstance(sexp, List):
+        if len(sexp) == 0:
+            raise ValueError("Missing function expression")
 
-		vals = [eval(i) for i in sexp]
-		import pytest; pytest.set_trace()
-		fun = vals[0]
-		return fun(*vals[1:])
+        vals = [eval(i) for i in sexp]
+        fun = vals[0]
+        return fun(vals[1:])
 
-	elif isinstance(sexp, Symbol):
-		return funs[sexp]
+    elif isinstance(sexp, Symbol):
+        return funs[sexp]
 
-	elif isinstance(sexp, Atom):
-		return sexp
-	
-	return sexp
+    elif isinstance(sexp, Atom):
+        return sexp
+
+    return sexp
+
 
 def read(tokens):
-	'''Lisp reader
-	'''
+    try:
+        tok = tokens[0]
+        tokens = tokens[1:]
+    except IndexError:
+        raise SyntaxError("Unexpected EOF")
 
-	try:
-		tok = tokens[0]
-		tokens = tokens[1:]
-	except IndexError:
-		raise SyntaxError("Unexpected EOF")
+    if tok == '(':
+        try:
+            end = tokens.index(')')
+        except ValueError:
+            raise SyntaxError("Expected ')'")
 
-	if tok == '(':
-		try:
-			end = tokens.index(')')
-		except ValueError:
-			raise SyntaxError("Expected ')'")
+        in_toks = tokens[:end]
 
-		in_toks = tokens[:end]
+        exp = List()
+        while in_toks:
+            exp.append(read(in_toks))
+            in_toks.pop(0)
 
-		exp = List()
-		while in_toks:
-			exp.append(read(in_toks))
-			in_toks.pop(0)
+        return exp
 
-		return exp
+    try:
+        atom = Number(tok)
+    except ValueError:
+        return Symbol(tok)
+    else:
+        return atom  # int after all
 
-	try:
-		atom = Number(tok)
-	except ValueError:
-		return Symbol(tok)
-	else:
-		return atom # int after all
 
 def tokenize(source):
-	'''Split by parens, consider commas whitespace
-	Inspired by Peter Norvig's lis.py
-	'''
+    '''Split by parens, consider commas whitespace
+    Inspired by Peter Norvig's lis.py
+    '''
 
-	tokens = {
-		'(': ' ( ',
-		')': ' ) ',
-		',': ' ',
-	}
+    tokens = {
+        '(': ' ( ',
+        ')': ' ) ',
+        ',': ' ',
+    }
 
-	for tok, rep in tokens.items():
-		source = source.replace(tok, rep)
+    for tok, rep in tokens.items():
+        source = source.replace(tok, rep)
 
-	return source.split()
+    return source.split()
