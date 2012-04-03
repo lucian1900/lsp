5a4baed25da70860c8286dd30e5de0b8bbbeffca
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-04-03 08:13:07 +0100

    Silly mistake in tests.

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-from lsp import tokenize, read, eval, Symbol
+from lsp import tokenize, read, eval, Symbol, List
 
 
 def test_tokenize():
-	assert tokenize("(1, 2)") == ['(', '1', '2', ')']
+    assert tokenize("(1, 2)") == ['(', '1', '2', ')']
 
 
 def test_read_atom():
-	assert read(['1']) == 1
+    assert read(['1']) == 1
 
 def test_read_list():
-	assert read(['(', '1', '2', ')']) == [1, 2]
+    assert read(['(', '1', '2', ')']) == [1, 2]
 
 def test_read_func():
-	assert read(['(', '+', '1', '2', ')']) == ['+', 1, 2]
+    assert read(['(', '+', '1', '2', ')']) == ['+', 1, 2]
 
 def test_read_unmatched():
-	pass
+    pass
 
 
 def test_eval_atom():
-	assert eval(1) == 1
+    assert eval(1) == 1
 
 def test_eval_func():
-	assert eval([Symbol('+'), 1, 2]) == 3
+    assert eval(List([Symbol('+'), 1, 2])) == 3
